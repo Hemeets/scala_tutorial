36b68bc04863089cfa36b55cfd4abeef8e1ba4a2
qdx_git <qdx_git@xxx.com>
2021-09-03 08:56:33 +0800

    0903

---
+++ aiguigu/src/main/scala/chapter07/test01_immutablearray.scala
@@ -0,0 +1,13 @@
+package chapter07
+
+object test01_immutablearray {
+  def main(args: Array[String]): Unit = {
+    // 1. 创建数组
+    val arr: Array[Int] = new Array[Int](5)
+
+    // 另一种创建的方式
+    val arr2 = Array(12, 37, 42, 58, 97)
+
+  }
+
+}
